6baa832c989120bba6312c99fe8818518fd9fd3b
Angela Netzke <angela.netzke@gmail.com>
2024-12-02 08:15:05 -0800

    Add 2024, Day 1

---
+++ 2024/program1.lua
@@ -0,0 +1,46 @@
+local function countOccurances(t, x)
+	local count = 0
+	for i = 1, #t do
+		if t[i] == x then
+			count = count + 1
+		end
+	end
+	return count
+end
+
+local function part1(leftList, rightList)
+	local differenceSum = 0
+	for i = 1, #leftList do
+		differenceSum = differenceSum + math.abs(leftList[i] - rightList[i])
+	end
+	return differenceSum
+end
+
+local function part2(leftList, rightList)
+	local similarityScore = 0
+	for i = 1, #leftList do
+		similarityScore = similarityScore + leftList[i] * countOccurances(rightList, leftList[i])
+	end
+	return similarityScore
+end
+
+io.input("input/input1.txt")
+local allLines = {}
+local i = 1
+local nextLine = io.read("*line")
+while nextLine ~= nil do
+	allLines[i] = nextLine
+	i = i + 1
+	nextLine = io.read("*line")
+end
+local leftList = {}
+local rightList = {}
+for i = 1, #allLines do
+	local _, _, left, right = string.find(allLines[i], "(%d+) +(%d+)")
+	leftList[i] = left
+	rightList[i] = right
+end
+table.sort(leftList)
+table.sort(rightList)
+print("Part 1: " .. part1(leftList, rightList))
+print("Part 2: " .. part2(leftList, rightList))
